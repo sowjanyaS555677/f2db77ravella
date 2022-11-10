8800a1287e5fdcc24e615b0cb891b17f9bc0d606
Sowjanya Ravella <111835425+sowjanyaS555677@users.noreply.github.com>
2022-11-09 21:40:49 -0600

    add

--- routes/Run.js
+++ routes/Run.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var Run_controlers= require('../controllers/Run');
 
 /* GET home page. */
 
-router.get('/', function (req, res, next) {
-    res.render('Run',{title:'Search Results Run'});
-});
+router.get('/', Run_controlers.Run_view_all_Page ); 
 
 module.exports = router;
