57cd6d319f81451250eb1ef50c851e0e424cda86
Sowjanya Ravella <111835425+sowjanyaS555677@users.noreply.github.com>
2022-11-29 23:27:13 -0600

    added files

--- models/Run.js
+++ models/Run.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose")
 const RunSchema = mongoose.Schema({
-Run_type: String,
-Run_age: Number,
-Run_rank: Number
+Run_type: {
+    type:String,
+    requrie:true
+
+},
+Run_age: {
+    type:Number,
+    min:2,
+    max:100
+},
+Run_rank: {
+    type:Number,
+    min:1,
+    max:50
+}
 })
 module.exports = mongoose.model("Run",
 RunSchema)
--- routes/resource.js
+++ routes/resource.js
@@ -4,6 +4,17 @@ var router = express.Router();
 var api_controller = require('../controllers/api');
 var Run_controller = require('../controllers/Run');
 var Run_controller_view = require('../controllers/Run_views');
+
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+ 
 /// API ROUTE ///
 // GET resources base.
 router.get('/', api_controller.api);
@@ -24,12 +35,12 @@ module.exports = router;
 router.get('/detail', Run_controller_view.Run_view_one_Page); 
 
 /* GET create costume page */ 
-router.get('/create', Run_controller_view.Run_create_Page); 
+router.get('/create', secured,Run_controller_view.Run_create_Page); 
 
 ///* GET create update page */ 
-router.get('/update', Run_controller_view.Run_update_Page); 
+router.get('/update', secured,Run_controller_view.Run_update_Page); 
 
 /* GET delete costume page */ 
-router.get('/delete', Run_controller_view.Run_delete_Page); 
+router.get('/delete', secured,Run_controller_view.Run_delete_Page); 
 
 
