2324b3cd9d25383bdfdcdf26aba137843b8d904b
Sowjanya Ravella <111835425+sowjanyaS555677@users.noreply.github.com>
2022-11-30 20:29:19 -0600

    added files

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,7 @@ router.get('/login', function(req, res) {
     res.render('login', { title: 'Run App Login', user : req.user }); 
 }); 
  
-router.post('/login', passport.authenticate('local'), function(req, res) { 
+router.post('/login', passport.authenticate('local',{keepSessionInfo:true}), function(req, res) { 
   if(req.session.returnTo) 
   res.redirect(req.session.returnTo); 
     res.redirect('/'); 
